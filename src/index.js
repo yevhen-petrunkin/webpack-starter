b7dabb80a1fb0be9a9382d5f1f2300cf817009a2
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-02-09 02:47:49 +0200

    Build trial version

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+import cat from '@/images/cat.png'
 
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
 const logo = document.createElement('img')
-logo.src = webpackLogo
+logo.src = cat
 
 const heading = document.createElement('h1')
 heading.textContent = example()
